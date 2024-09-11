6eaf2011e67155cb806bb12e8a748d0c146469ea
suous <111222500+suous@users.noreply.github.com>
2024-09-11 11:07:57 +0800

    updated view entrypoint.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ import { drawerWidth } from './constants';
 
 const Container = styled('main', { shouldForwardProp: prop => prop !== 'open' })<{
   open?: boolean;
-}>(({ theme, open }) => ({
+}>(({ theme }) => ({
   flexGrow: 1,
   padding: theme.spacing(1),
   transition: theme.transitions.create('margin', {
@@ -26,13 +26,18 @@ const Container = styled('main', { shouldForwardProp: prop => prop !== 'open' })
     duration: theme.transitions.duration.leavingScreen
   }),
   marginLeft: `-${drawerWidth}px`,
-  ...(open === true && {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginLeft: 0
-  })
+  variants: [
+    {
+      props: ({ open }) => open,
+      style: {
+        transition: theme.transitions.create('margin', {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen
+        }),
+        marginLeft: 0
+      }
+    }
+  ]
 }));
 
 export default function Home(): JSX.Element {
@@ -54,18 +59,16 @@ export default function Home(): JSX.Element {
   }, [drawer, updateDrawer]);
 
   return (
-    <>
+    <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <Box sx={{ display: 'flex' }}>
-        <Drawer />
-        <Container open={drawer}>
-          <Main />
-        </Container>
-        <Backdrop sx={{ color: '#fff', zIndex: theme => theme.zIndex.drawer + 1 }} open={loading}>
-          <CircularProgress color='inherit' />
-        </Backdrop>
-      </Box>
+      <Drawer />
+      <Container open={drawer}>
+        <Main />
+      </Container>
+      <Backdrop sx={{ color: '#fff', zIndex: theme => theme.zIndex.drawer + 1 }} open={loading}>
+        <CircularProgress color='inherit' />
+      </Backdrop>
       <Preference />
-    </>
+    </Box>
   );
 }
